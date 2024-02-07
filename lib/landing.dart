1e8b0f374826f60bd28580edd335a916a8649912
Onwuka Daniel <onwukadaniel16@gmail.com>
2024-02-07 19:41:57 +0100

    Initial commit

---
+++ lib/landing.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Landing extends StatelessWidget {
+  const Landing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(16),
+        children: [
+        ],
+      ),
+    );
+  }
+}
