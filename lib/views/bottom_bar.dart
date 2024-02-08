7650a036cc64da158cb29ceed438b59f6ec1bec4
Onwuka Daniel <onwukadaniel16@gmail.com>
2024-02-08 06:33:16 +0100

    Make bottom_bar responsive to clicks

--- lib/views/bottom_bar.dart
+++ lib/views/bottom_bar.dart
@@ -6,12 +6,13 @@ class BottomBar extends StackedHookView<LandingViewModel> {
   @override
   Widget builder(BuildContext context, LandingViewModel model) {
     double width = MediaQuery.of(context).size.width;
+    double viewHeight = 64;
     double screenPadding = 16;
     return Positioned(
       bottom: 1,
       width: width,
       child: Container(
-        height: 64,
+        height: viewHeight,
         margin: EdgeInsets.all(screenPadding),
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
@@ -23,8 +24,42 @@ class BottomBar extends StackedHookView<LandingViewModel> {
             model.bottomBarList.length,
             (index) {
               var data = model.bottomBarList[index];
+              if (model.bottomTab == index) {
+                return Container(
+                  height: viewHeight - 25,
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  margin: const EdgeInsets.symmetric(horizontal: 8),
+                  decoration: BoxDecoration(
+                    color: AppColor.orange,
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 12),
+                      Image.asset(data.imagePath, height: 16, width: 16),
+                      const SizedBox(width: 12),
+                      Text(
+                        data.title,
+                        softWrap: true,
+                        maxLines: 2,
+                        style: TextStyle(fontSize: 13, color: AppColor.black1),
+                      ),
+                      const SizedBox(width: 12),
+                    ],
+                  ),
+                );
+              }
               return Expanded(
-                child: Image.asset(data.imagePath, height: 16, width: 16,),
+                child: InkWell(
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () => model.setBottomTab(index),
+                  child: Image.asset(
+                    data.imagePath,
+                    height: 16,
+                    width: 16,
+                  ),
+                ),
               );
             },
           ),
